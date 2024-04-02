0b14e9018f8091924f87b83c6970a7d15e5547cf
ARM <armreshad@gmail.com>
2024-04-02 18:14:26 +0300

    bug fix on db changed the charset to utf8mb64 for emoji acceptenc

--- reviews/review.data-source.ts
+++ reviews/review.data-source.ts
@@ -1,17 +1,18 @@
-import { DataSource } from "typeorm"
+import { DataSource } from 'typeorm';
 import { Comment, ReactionComment, ReactionReview, Review } from '../core/entities';
 
 const dataSource = new DataSource({
-    type: "mysql",
-    host: process.env.MYSQL_HOST ?? 'localhost',
-    port: 3306,
-    username: "root",
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    logging: true,
-    synchronize: true,
-    migrationsRun: false,
-    entities: [Review, Comment, ReactionComment, ReactionReview],
+  type: 'mysql',
+  host: process.env.MYSQL_HOST ?? 'localhost',
+  port: 3306,
+  username: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  logging: true,
+  synchronize: true,
+  migrationsRun: false,
+  charset: 'utf8mb4',
+  entities: [Review, Comment, ReactionComment, ReactionReview],
 });
 
 export default dataSource;
