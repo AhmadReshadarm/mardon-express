1027921d9465809d133121aa75a63ff75e3559c7
ARM <armreshad@gmail.com>
2024-11-09 15:21:28 +0300

    extended the token duration

--- authorization/functions/access.token.ts
+++ authorization/functions/access.token.ts
@@ -2,5 +2,5 @@ import * as jwt from 'jsonwebtoken';
 
 export const accessToken = (payload: object) => {
   const { ACCESS_SECRET_TOKEN } = process.env;
-  return jwt.sign(payload, ACCESS_SECRET_TOKEN, { expiresIn: '2 hours' });
+  return jwt.sign(payload, ACCESS_SECRET_TOKEN, { expiresIn: '1 day' });
 };
