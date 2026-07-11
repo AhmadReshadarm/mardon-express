a1b5a1a8217933694556d4ada33ee8b34f6aff95
ARM <armreshad@gmail.com>
2026-07-11 15:16:04 +0300

    preview config

--- reviews/review/review.controller.ts
+++ reviews/review/review.controller.ts
@@ -118,17 +118,17 @@ export class ReviewController {
     }
   }
 
-  @Get('one-time-token/:publicKey')
-  @Middleware([verifyToken, isAdmin])
-  async generateOneTimeToken(req: Request, resp: Response) {
-    try {
-      const { publicKey } = req.params;
-      const created = oneTimeToken({ publicKey });
-      resp.status(HttpStatus.CREATED).json({ token: created });
-    } catch (error) {
-      resp.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
-    }
-  }
+  // @Get('one-time-token/:publicKey')
+  // @Middleware([verifyToken, isAdmin])
+  // async generateOneTimeToken(req: Request, resp: Response) {
+  //   try {
+  //     const { publicKey } = req.params;
+  //     const created = oneTimeToken({ publicKey });
+  //     resp.status(HttpStatus.CREATED).json({ token: created });
+  //   } catch (error) {
+  //     resp.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
+  //   }
+  // }
 
   @Post('reaction')
   @Middleware([verifyToken, isUser])
